99669ae5d4919498dcc900fcd5461cf69c315b9d
Amy Lee <amy.lee@c3iot.com>
2017-09-28 11:15:31 -0700

    change github OAuth app to local dev

--- knowledge_repo/app/config_defaults.py
+++ knowledge_repo/app/config_defaults.py
@@ -10,7 +10,7 @@ import datetime
 # not provided, it is assumed in these contexts to be
 # 'localhost:7000'. Be sure to specify this before deploying
 # into production.
-SERVER_NAME = 'knowledge.c3iot.com' # full url or host port? TODO look at config file argument, pass in different
+SERVER_NAME = None #'knowledge.c3iot.com'
 
 # The knowledge repository uses the secret key to sign user
 # sessions. If not specified, a unique secret key will be
@@ -58,8 +58,8 @@ AUTH_PROVIDERS = ['github']
 # and private tokens. This can be done by instantiating instances of
 # `OAuth2Provider` and adding them to the above list, or by specifying OAuth
 # connection properties as demonstrated below for the GitHub authenticator.
-OAUTH_GITHUB_CLIENT_ID = '839533a2dd7f175bd54e'
-OAUTH_GITHUB_CLIENT_SECRET = 'fc3e94ffa49ec92d324c4d33ab8b94dcf0268b5b'
+OAUTH_GITHUB_CLIENT_ID = 'aa34838067f809f089cf'
+OAUTH_GITHUB_CLIENT_SECRET = '49770a27a2dadb7994d3d8c5f14c73e0fbc7d8e4'
 
 # You can also forgo a fully-fledged sign in process for users
 # by hosting the knowledge repository behind a proxy server that
