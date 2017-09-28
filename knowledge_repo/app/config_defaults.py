153bfc0163957ee625931d82edd9975e5958dc35
Amy Lee <amy.lee@c3iot.com>
2017-09-28 09:13:29 -0700

    update server name

--- knowledge_repo/app/config_defaults.py
+++ knowledge_repo/app/config_defaults.py
@@ -10,7 +10,7 @@ import datetime
 # not provided, it is assumed in these contexts to be
 # 'localhost:7000'. Be sure to specify this before deploying
 # into production.
-SERVER_NAME = None
+SERVER_NAME = 'knowledge.c3iot.com' # full url or host port?
 
 # The knowledge repository uses the secret key to sign user
 # sessions. If not specified, a unique secret key will be
